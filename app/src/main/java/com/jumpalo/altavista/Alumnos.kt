28a5da8b377832febcc4b35dcfafa5ce3027f600
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-10-25 21:23:39 -0300

    dni agregado. ahora crea alumno vacio si no hay datos.

--- app/src/main/java/com/jumpalo/altavista/Alumnos.kt
+++ app/src/main/java/com/jumpalo/altavista/Alumnos.kt
@@ -1,20 +1,23 @@
 package com.jumpalo.altavista
 
 class Alumnos(
-    var Carnet:String,
-    var Nombre:String,
-    var Apellido:String,
-    var Curso:String,
-    var Division:String,
-    var Turno:String,
-    var hasImg:Int
+    var Carnet:String="",
+    var Nombre:String="",
+    var Apellido:String="",
+    var Curso:String="",
+    var Division:String="",
+    var Turno:String="",
+    var Dni:String,
+    var hasImg:Int=0
 ){
-    override fun toString() = "$Carnet,$Nombre,$Apellido,$Curso,$Division,$Turno,$hasImg"
+    override fun toString() = "$Carnet,$Nombre,$Apellido,$Curso,$Division,$Turno,$Dni,$hasImg"
     constructor(str : String){
-        val data = str.split(',')
-        Carnet = data[0];
-        Nombre = data[1]; Apellido = data[2]
-        Curso = data[3]; Division = data[4]
-        Turno = data[5]; hasImg = data[6].toInt()
+        if(str!="0"){
+            val data = str.split(',')
+            Carnet = data[0]; Dni = data[6]
+            Nombre = data[1]; Apellido = data[2]
+            Curso = data[3]; Division = data[4]
+            Turno = data[5]; hasImg = data[7].toInt()
+        }
     }
 }
