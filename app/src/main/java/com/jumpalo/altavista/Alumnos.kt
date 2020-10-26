d6f4205d61e852bc10f537eb1e2869cd982a9446
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-10-25 21:09:10 -0300

    Alumnos toString and constructor fromString Datos irrelevantes eliminados

--- app/src/main/java/com/jumpalo/altavista/Alumnos.kt
+++ app/src/main/java/com/jumpalo/altavista/Alumnos.kt
@@ -4,9 +4,17 @@ class Alumnos(
     var Carnet:String,
     var Nombre:String,
     var Apellido:String,
-    var Sexo:String,
-    var Edad:String,
     var Curso:String,
     var Division:String,
+    var Turno:String,
     var hasImg:Int
-)
+){
+    override fun toString() = "$Carnet,$Nombre,$Apellido,$Curso,$Division,$Turno,$hasImg"
+    constructor(str : String){
+        val data = str.split(',')
+        Carnet = data[0];
+        Nombre = data[1]; Apellido = data[2]
+        Curso = data[3]; Division = data[4]
+        Turno = data[5]; hasImg = data[6].toInt()
+    }
+}
